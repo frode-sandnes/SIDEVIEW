4ebfd21a2e81a7a8a11d4244a95ea422dec7d9e4
frode-sandnes <79444835+frode-sandnes@users.noreply.github.com>
2022-03-23 13:10:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -53,6 +53,40 @@ async function viewing()
 
 async function setup() 
   {
+// console log to document
+// Reference to an output container, use 'pre' styling for JSON output
+var output = document.createElement('pre');
+document.body.appendChild(output);
+
+// Reference to native method(s)
+var oldLog = console.log;
+
+console.log = function( ...items ) {
+
+    // Call native method first
+    oldLog.apply(this,items);
+
+    // Use JSON to transform objects, all others display normally
+    items.forEach( (item,i)=>{
+        items[i] = (typeof item === 'object' ? JSON.stringify(item,null,4) : item);
+    });
+    output.innerHTML += items.join(' ') + '<br />';
+
+};
+console.log("Starting");
+
+/*// You could even allow Javascript input...
+function consoleInput( data ) {
+    // Print it to console as typed
+    console.log( data + '<br />' );
+    try {
+        console.log( eval( data ) );
+    } catch (e) {
+        console.log( e.stack );
+    }
+}*/
+
+
 
     // To get maximul resulution set up ideal dimensions
     var constraints = 
